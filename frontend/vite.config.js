0fc6a109a94d21ba34f4e82cc328d4c63aa97b6f
BLACKINK\nunod <nunodacostaramos@gmail.com>
2025-10-11 18:06:40 +0100

    - Adapted vite  config.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import fs from "node:fs";
 import path from "node:path";
@@ -7,18 +7,24 @@ import { fileURLToPath } from "node:url";
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
+    const isDev = mode === "development";
 
     return {
+        base: isDev ? "/" : "/PersonalFinance/",
+
         plugins: [vue()],
-        server: {
-            host: "localhost",
-            port: 5173,
-            https: {
-                key: fs.readFileSync(path.resolve(__dirname, "../certs/localhost-key.pem")),
-                cert: fs.readFileSync(path.resolve(__dirname, "../certs/localhost.pem")),
-            },
-        },
+
+        server: isDev
+            ? {
+                host: "localhost",
+                port: 5173,
+                https: {
+                    key: fs.readFileSync(path.resolve(__dirname, "../certs/localhost-key.pem")),
+                    cert: fs.readFileSync(path.resolve(__dirname, "../certs/localhost.pem")),
+                },
+            }
+            : undefined,
+
         resolve: {
             alias: { "@": path.resolve(__dirname, "./src") },
         },
